94f8a1de841a06851127d76ecfa8386c3b0fe84d
Ahmad <ahmadehsas14345@gmail.com>
2025-07-21 23:21:28 +0100

    Udate the parameter to the function and state the syntax to destructure the object.

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -4,9 +4,12 @@ const personOne = {
   favouriteFood: "Spinach",
 };
 
+// The syntax to  destructure the object `personOne`.
+const { name, age, favouriteFood } = personOne;
+
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
